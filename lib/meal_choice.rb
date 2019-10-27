6372aa37e9d1f0d173c5a38ad26ce0310e4dedf8
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 13:57:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,7 @@
 
 def meal_choice(protein = {}, veg1, veg2)
     [protein = "Feasant"]
+    puts "protein" + "veg1 + veg2"
     puts "What a nutritious meal!"
     puts "A plate of #{protein} with #{veg1} and #{veg2}.""so that you can see what you've ordered."
     return "A plate of #{protein} with #veg1} and #{veg2}."
