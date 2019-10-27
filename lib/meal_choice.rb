b007f20949b5426412097d6f7b3b67f968402fde
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 15:25:24 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,9 @@
 # Your code here
 # Define a Method that takes in/ uses a required and optional Argument 
 
-def meal_choice(protein = "meat", veg1, veg2) #Meat
+def meal_choice(protein = "meat", veg1, veg2) 
     puts "What a nutritious meal!"
-    puts "A plate of #{protein} with #{veg1} and #{veg2}.""so that you can see what you have ordered."
+    puts "A plate of #{protein} with #{veg1} and #{veg2}."
     return "A plate of #{protein} with #{veg1} and #{veg2}."
     
 end
