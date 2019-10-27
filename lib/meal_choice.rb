027bec2e4079163bf0c2396073c5a4e37ef4d11a
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 14:11:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,7 +8,7 @@ def meal_choice(protein = {}, veg1, veg2) #Meat
     puts "A plate of #{protein} with #{veg1} and #{veg2}.""so that you can see what you've ordered."
     return "A plate of #{protein} with #veg1} and #{veg2}."
 end
-
+end
   meal_choice
   meal_choice("Feasant","Tuscan Kale", "Sunchoke Salad")
   meal_choice("Tuscan Kale", "Sunchoke Salad") 
